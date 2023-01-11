736ab870db34045fc92e5396d99d776e22b6a364
Pravinbadgu2311 <96545733+Pravinbadgu2311@users.noreply.github.com>
2023-01-11 11:02:29 +0530

    first commit

---
+++ src/constant.js
@@ -0,0 +1,6 @@
+//module.exports.USER = 'user';
+module.exports.ADMIN = 'admin';
+module.exports.DISTRIBUTOR = 'distributor';
+module.exports.CONTRACTOR = 'contractor';
+
+module.exports.AUTH_TOKEN = 'x-auth-token';
